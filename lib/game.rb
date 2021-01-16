b5a3c654548265d81d9d1dcdc87637cd59d1ee3c
Luis Martinez <luis_mmartinez@live.com>
2021-01-16 17:30:49 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -47,7 +47,7 @@ class Game
   def turn
         #METHOD 1: SHORT METHOD
     input=current_player.move(board)              #"Current_player.move(board)"" asks for input from player_1
-    board.update(input,current_player) || turn
+    board.update(input, current_player) || turn
         #METHOD 2: LONG METHOD
   #  input=current_player.move(board)
   #  if board.update(input,current_player)
