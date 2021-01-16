937f7b709b80128e13da5ead613993ee1cd6a471
Luis Martinez <luis_mmartinez@live.com>
2021-01-16 21:34:27 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -49,6 +49,9 @@ class Players::Computer
 
         else
           input= "5"
+        end
+      end
+      board.update(input, self)
         #  me = array.detect {|i| board.cells[i] == self.token || (board.cells[i] != self.token && board.cells[i] != " ")}
       #    raw_input = array.detect {|i| board.cells[i] == self.token || (board.cells[i] != self.token && board.cells[i] != " ")}
 
@@ -69,7 +72,7 @@ class Players::Computer
         end
 
       end
-      board.update(input, self)
+      
       input
   end
 
